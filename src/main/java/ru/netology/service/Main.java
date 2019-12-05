60bde1499062e6c051f9648b7bb01558e8b8f440
AleksandrZhuravel <cah40c@tut.by>
2019-12-05 13:31:17 +0300

    Jacoco added, java code changed

--- src/main/java/ru/netology/service/Main.java
+++ src/main/java/ru/netology/service/Main.java
@@ -3,7 +3,7 @@ package ru.netology.service;
 public class Main {
     public static void main(String[] args) {
         CashbackHackService service = new CashbackHackService();
-        int result = service.remain(1000);
+        int result = service.remain(1100);
         System.out.println(result);
     }
 
