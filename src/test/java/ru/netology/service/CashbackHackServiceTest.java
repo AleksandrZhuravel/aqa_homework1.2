ecbcd66ec945c82272941916d59eafee82d40fb8
AleksandrZhuravel <cah40c@tut.by>
2019-12-06 13:00:19 +0300

    Parameterized added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,45 +1,48 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CashbackHackServiceTest {
+
     @ParameterizedTest
-
-    void shouldReturn100IfAmountIs900() {
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    @DisplayName("Should be 100 for amount 900")
+    void shouldReturn100IfAmountIs900(int amount, int expected) {
         CashbackHackService cashbackHackService100 = new CashbackHackService();
-        int amount = 900;
         int actual = cashbackHackService100.remain(amount);
-        int expected = 100;
         assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldReturn0IfAmountIs1000() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    @DisplayName("Should be 0 for amount 1000")
+    void shouldReturn0IfAmountIs1000(int amount, int expected) {
         CashbackHackService cashbackHackService0 = new CashbackHackService();
-        int amount = 1000;
         int actual = cashbackHackService0.remain(amount);
-        int expected = 0;
         assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldReturn500IfAmountIs500() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    @DisplayName("Should be 500 for amount 500")
+    void shouldReturn500IfAmountIs500(int amount, int expected) {
         CashbackHackService cashbackHackService500 = new CashbackHackService();
-        int amount = 500;
         int actual = cashbackHackService500.remain(amount);
-        int expected = 500;
         assertEquals(expected, actual);
     }
 
 
     @Test
+    @DisplayName("Should be exeption for amount 0")
     void shouldReturnThrowIfAmountIs0() {
-        CashbackHackService cashbackHackServiceThrow = new CashbackHackService();
-        assertThrows(IllegalArgumentException.class, () -> cashbackHackServiceThrow.remain(amount))
+        CashbackHackService one = new CashbackHackService();
+        assertThrows(IllegalArgumentException.class, () -> one.remain(0));
     }
 
 }
